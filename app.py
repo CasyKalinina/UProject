11631356b73b996c70660b10935f0b9f8db6c253
CasyKalinina <55532668+CasyKalinina@users.noreply.github.com>
2019-10-16 22:33:40 +0300

    lab1

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, redirect, url_for
+from main import parser
+import json
+
+app = Flask(__name__)
+page_url = 'https://vc.ru'
+
+
+@app.route('/')
+def start():
+    parser.publish_report('/Users/apple/PycharmProjects/2019-3-level-labs/lab_1/main/articles.json',
+                          parser.find_articles(
+                              parser.get_html_page('https://vc.ru')))
+    with open("/Users/apple/PycharmProjects/2019-3-level-labs/lab_1/main/articles.json", "r") as read_file:
+        data = json.load(read_file)
+        link = data["url"]
+        articles = data["articles"]
+    return render_template('report.html', link=link, list=articles)
+@app.route('/refresh', methods=['POST'])
+
+
+def refresh():
+    return redirect(url_for('start'))
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=5000, debug=True)
